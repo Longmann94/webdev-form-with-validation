c610c19256cc3d34bef1bf3e068db2ce7f70de53
Long Mann <layfon@live.com.au>
2022-05-30 16:34:45 +1000

    update various css style for inputs add js to check for matched passwords

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+function checkForm(){
+  const first_pass = document.getElementById("pass").value;
+  const second_pass = document.getElementById("pass_confirm").value;
+
+  if(first_pass !== second_pass){
+    //if both pass do NOT match
+    //set the input to style as invalid
+    document.getElementById("pass").setCustomValidity("Invalid field");
+    document.getElementById("pass_confirm").setCustomValidity("Invalid field");
+    //add display type to the error message div to show it
+    document.getElementById("password-notmatched").style.display = "flex";
+  }else {
+    //if both pass are macthing
+    //set the input to style to valid
+    document.getElementById("pass").setCustomValidity("");
+    document.getElementById("pass_confirm").setCustomValidity("");
+    //add style to message div to hide it
+    document.getElementById("password-notmatched").style.display = "none";
+
+  }
+
+}
+
+window.addEventListener("keyup", checkForm);
